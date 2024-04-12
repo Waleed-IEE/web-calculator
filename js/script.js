a6931401bdd90d49acc84d5637dc1ebbf56e363f
Waleed-IEE <iee.twaleed@gmail.com>
2024-04-13 00:52:59 +0100

    feat(js): Push the digits of the clicked number to either the variable leftOperand or rightOperand depending on a condition

--- js/script.js
+++ js/script.js
@@ -25,7 +25,18 @@ const buttons = document.querySelector(".buttons.container");
 
 buttons.addEventListener("click", (event) => {
     if(event.target.tagName === "BUTTON"){
-        if((typeof +event.target.textContent === "number" && !isNaN(+event.target.textContent)) || event.target.textContent === "."){}
+        if((typeof +event.target.textContent === "number" && !isNaN(+event.target.textContent)) || event.target.textContent === "."){
+            if(mathOperator === ""){
+                if(!(leftOperand.length === 0 && event.target.textContent === "0")){
+                    leftOperand.push(event.target.textContent);
+                }
+            }
+            else{
+                if(!(rightOperand.length === 0 && event.target.textContent === "0")){
+                    rightOperand.push(event.target.textContent);
+                }
+            }
+        }
         else if(event.target.textContent === "clear"){
             leftOperand = [];
             mathOperator = "";
